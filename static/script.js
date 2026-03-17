1ab841d271f3ccc15ca4ae598d715c02b0a4fe2e
Chon211205 <lee241203@uvg.edu.gt>
2026-03-17 16:59:14 -0600

    presionar enter para enviar mensaje agregado

--- static/script.js
+++ static/script.js
@@ -1,32 +1,51 @@
 const getMessages = async () =>{
-        const response = await fetch("/api/messages")
-        const messages = await response.json()
-        console.log("messages",messages)
-        const ul= document.getElementById("messages")
-        ul.innerHTML=''
-        for(let i =0; i<messages.length;i++){
-            const message = messages[i]
-            const li =document.createElement('li')
-            li.innerHTML=`<strong> ${message.user}:</strong> ${message.text}`
-            console.log("message",message)
-            ul.append(li)
-        }
+    const response = await fetch("/api/messages")
+    const messages = await response.json()
+    console.log("messages",messages)
+
+    const ul= document.getElementById("messages")
+    ul.innerHTML=''
+
+    for(let i =0; i<messages.length;i++){
+        const message = messages[i]
+        const li =document.createElement('li')
+        li.innerHTML=`<strong>${message.user}:</strong> ${message.text}`
+        ul.append(li)
     }
+}
 
-    const postMessages = async (message) =>{
-        await fetch("/api/messages", {
-            method: 'POST', body: JSON.stringify(message) }) 
-            getMessages()
-
-    }
-
+const postMessages = async (message) =>{
+    await fetch("/api/messages", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    }) 
     getMessages()
+}
 
-    document.getElementById('send').addEventListener('click', () => {
-        const message = document.getElementById('message').value
-        postMessages({
-        user: 'user',
-        text: message
-        })
+function sendMessage(){
+    const textarea = document.getElementById('message')
+    const text = textarea.value.trim()
+
+    if(!text) return
+
+    postMessages({
+        user: 'chon',
+        text: text
+    })
+
+    textarea.value = "" // limpiar input
+}
+
+document.getElementById('send').addEventListener('click', sendMessage)
+
+document.getElementById('message').addEventListener('keydown', (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault()
+        sendMessage()
     }
-)
+})
+
+getMessages()
